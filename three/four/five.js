16b00c93a2f1cb19e7c422dbf9554344852fa69c
Rahulkoli1998 <rishirahul19998@gmail.com>
2025-06-11 15:34:28 +0530

    new feature is added by akhilesh in one.js

--- three/four/five.js
+++ three/four/five.js
@@ -3,3 +3,5 @@ console.log("kya hua pandey jiii add nhi hua kya")
 console.log("agar sirf file of staging area me leke jana h to uska actual path add krna hoga")
 
 console.log("agr pura folder add kiya to uske andr ka sab kuch stagging area me aa jaega")
+
+console.log("akhilesh ne isme bhi kuch nya feature add kiya hai ")
