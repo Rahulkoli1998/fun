995b73b003a6090bff293179b9997ff4fbc2cfb6
Rahulkoli1998 <rishirahul19998@gmail.com>
2025-06-11 12:28:27 +0530

    inital commit only to for five.js file

---
+++ three/four/five.js
@@ -0,0 +1,5 @@
+console.log("kya hua pandey jiii add nhi hua kya")
+
+console.log("agar sirf file of staging area me leke jana h to uska actual path add krna hoga")
+
+console.log("agr pura folder add kiya to uske andr ka sab kuch stagging area me aa jaega")
