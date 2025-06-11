19b70b7556b329312aae54ed8644618ff6507cba
Rahulkoli1998 <rishirahul19998@gmail.com>
2025-06-11 12:31:50 +0530

    Now we are adding all files/ folder from stagging area

---
+++ one/one.js
@@ -0,0 +1,8 @@
+const a = 5;
+const b = 10;
+
+function myfunc(){
+    return a+b
+}
+
+console.log(myfunc())
---
+++ three/four/four.js
@@ -0,0 +1,10 @@
+let a = "Rahul";
+let  b = "koli";
+const c = 5
+
+function concatination(){
+    return a + b + c
+}
+
+
+console.log(concatination())
