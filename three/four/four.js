1404f33e44fa7117eaa53ce10b60a7effaeeffbe
Rahulkoli1998 <rishirahul19998@gmail.com>
2025-06-11 12:53:28 +0530

    added the isAbove function in four.js file

--- three/four/four.js
+++ three/four/four.js
@@ -2,9 +2,17 @@ let a = "Rahul";
 let  b = "koli";
 const c = 5
 
+let age = 15
+
 function concatination(){
     return a + b + c
 }
+
+function isAbove(){
+    return age <= 18;
+}
+
+console.log(isAbove())
 
 
 console.log(concatination())
